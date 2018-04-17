80923334dac05c80b70ed50502d24ec354401145
decipherintl <myles@decipherintl.com>
2018-04-17 16:45:00 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,36 +1,3 @@
-<script>
- alert('process');
-</script>
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["msg"];
- 
-$EmailTo = "amitacomval@gmail.com";
-$Subject = "New Message Received";
- 
-// prepare email body text
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
- 
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-
-
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
- 
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
-// redirect to success page
-if (isset($success)){
-   echo "success";
-}else{
-    echo "invalid";
-}
- 
+echo "ok";
 ?>
