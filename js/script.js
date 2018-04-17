d261a99a71ff7af9ae1c52145533a00ff7e90e4e
decipherintl <myles@decipherintl.com>
2018-04-17 16:29:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function submitForm(){
         url: "process.php",
         data: "name=" + name + "&email=" + email + "&msg=" + message,
         success : function(text){
-            alert(text);
+            alert('text');
         }
 	    
 	   
